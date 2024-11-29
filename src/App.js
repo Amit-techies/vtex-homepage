0c7076179380ca4bd1a2faee309ebb7084633e41
Amit-techies <amit.singh@iamtechie.com>
2024-11-29 10:46:23 +0530

    customer field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './components/banner';
 import Header from './components/Header';
+import CustomerForm from './components/CustomerForm';
 
 const App = () => {
   const defaultImage = 'https://via.placeholder.com/200'; // Placeholder image if no product image
