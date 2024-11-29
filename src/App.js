c2b51561292f16a02d8f554518946ab1643605ec
Amit-techies <amit.singh@iamtechie.com>
2024-11-29 11:37:24 +0530

    customer field pagenew

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './components/banner';
 import Header from './components/Header';
 import CustomerForm from './components/CustomerForm';
 import CustomerProfile from './components/CustomerProfile';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   const defaultImage = 'https://via.placeholder.com/200'; // Placeholder image if no product image
